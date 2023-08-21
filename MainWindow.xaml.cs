0dc575cde33c62de72ccb7d61d2155d6637d5ecb
hibel99 <111115004+hibel99@users.noreply.github.com>
2023-08-21 15:27:34 +0300

    updates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace InputsApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
