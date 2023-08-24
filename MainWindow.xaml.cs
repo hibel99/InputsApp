57706d5a5102c10f6f139946b324b64aebd819f1
hibel99 <111115004+hibel99@users.noreply.github.com>
2023-08-24 11:09:16 +0300

    ....

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace InputsApp
 
         private readonly IPivotPartsRepository _pivotPartsRepository;
         private readonly ISprinklerPartsRepository _sprinklerPartsRepository;
-
+        private SprinklerParts sprinklerEdit = null;
+        private PivotParts pivotEdit = null;
 
 
         public MainWindow()
@@ -33,6 +34,7 @@ namespace InputsApp
             _sqlDataAccess = new SqlDataAccess();
             _pivotPartsRepository = new PivotPartsRepository(_sqlDataAccess);
             _sprinklerPartsRepository = new SprinklerPartsRepository(_sqlDataAccess);
+            UpdateGrid();
         }
 
 
@@ -46,18 +48,249 @@ namespace InputsApp
             this.WindowState = WindowState.Minimized;
         }
 
-        private async void pivotPartsGrid_Loaded(object sender, RoutedEventArgs e)
-        {
 
-            var pivotPart = new PivotParts(6, "TestCat6FromEdit", "PivotTestName3FromEdit", 122, DateTime.Now, 20, 20, 20, 20);
-            //var sprinklerPart = new SprinklerParts(1,"sprinklerPartCatFromEdit", "sprinklerTestName1FromEdit", 122, DateTime.Now, 25, 20, 20, 20);
-            //await _sprinklerPartsRepository.AddSprinklerPart(sprinklerPart);
-            //await _pivotPartsRepository.DeletePivotPart(2);
-            //await _pivotPartsRepository.AddPivotPart(pivotPart);
-            //await _sprinklerPartsRepository.EditSprinklerPart(sprinklerPart);
+        async private void DeleteButtoninDG_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this item?", "Confirm Delete", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                PivotParts part = (PivotParts)((Button)sender).DataContext;
+
+                await _pivotPartsRepository.DeletePivotPart(part.ID);
+
+                UpdateGrid();
+            }
+        }
+
+        async private void UpdateGrid()
+        {
+            if ((bool)Pivot.IsChecked)
+            {
+                var result = await _pivotPartsRepository.GetPivotParts();
+                pivotPartsGrid.ItemsSource = result;
+            }
+            if ((bool)SpinklersParts.IsChecked)
+            {
+                var result = await _sprinklerPartsRepository.GetSprinklerParts();
+                SprinklerPartsDG.ItemsSource = result;
+            }
+            
+        }
+
+        private async void AddPivot_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (pivotEdit != null)
+            {
+                UpdateItemFromTextBoxes_Pivots(pivotEdit);
+            }
+            else
+            {
+                var pivotPart = new PivotParts(
+                PivotCategoryCB.Text,
+                PivotPartTB.Text,
+                decimal.Parse(pivotCostTB.Text),
+                DateTime.UtcNow,
+                decimal.Parse(pivotHegitTB.Text),
+                decimal.Parse(pivotwidthTB.Text),
+                decimal.Parse(pivotlenghtTB.Text),
+                decimal.Parse(pivotWeightTB.Text));
+
+
+                await _pivotPartsRepository.AddPivotPart(pivotPart);
+                UpdateGrid();
+            }
+
+            ClearTextBoxes();
+        }
+
+        private async void AddSpinkler_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sprinklerEdit != null)
+            {
+                UpdateItemFromTextBoxes_Sprinklers(sprinklerEdit);
+            }
+            else
+            {
+                var sprinklerPart = new SprinklerParts(
+                SprinklerCategoryCB.Text,
+                SprinklerPartTB.Text,
+                decimal.Parse(SprinklerCostTB.Text),
+                DateTime.UtcNow,
+                decimal.Parse(SprinklerHeightTB.Text),
+                decimal.Parse(SprinklerwidthTB.Text),
+                decimal.Parse(SprinklerlengthTB.Text),
+                decimal.Parse(SprinklerWeightTB.Text)
+                );
+
+                await _sprinklerPartsRepository.AddSprinklerPart(sprinklerPart);
+                UpdateGrid();
+            }
+
+            ClearTextBoxes();
+        }
+
+
+
+        private void NumericTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            // Check if the input text is numeric or a decimal point
+            foreach (char c in e.Text)
+            {
+                if (!char.IsDigit(c) && c != '.')
+                {
+                    e.Handled = true; // Prevent non-numeric characters from being entered
+                    break;
+                }
+            }
+
+            // Ensure only one decimal point is allowed
+            if (e.Text == "." && textBox.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        async private void DeleteButtoninSPrinklerDG_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this item?", "Confirm Delete", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                PivotParts part = (PivotParts)((Button)sender).DataContext;
+
+                await _sprinklerPartsRepository.DeleteSprinklerPart(part.ID);
+
+                UpdateGrid();
+            }
+        }
+
+        private void Pivot_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateGrid();
+        }
+
+        private void SpinklersParts_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateGrid();
+        }
+
+        private void SprinklerPartsDG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SprinklerPartsDG.SelectedItem is SprinklerParts selectedItem)
+            {
+                sprinklerEdit = selectedItem;
+                UpdateTextBoxesFromItem_Sprinklers(sprinklerEdit);
+            }
+            else
+            {
+                sprinklerEdit = null;
+                ClearTextBoxes();
+            }
+        }
+        private void UpdateTextBoxesFromItem_Sprinklers(SprinklerParts sprinklerEdit)
+        {
+            SprinklerCategoryCB.Text = sprinklerEdit.SprinklerCategory;
+            SprinklerPartTB.Text = sprinklerEdit.SprinklerPart;
+            SprinklerCostTB.Text = sprinklerEdit.Cost.ToString();
+            SprinklerDatePicker.SelectedDate = sprinklerEdit.Date;
+            SprinklerHeightTB.Text = sprinklerEdit.Height.ToString();
+            SprinklerwidthTB.Text = sprinklerEdit.Width.ToString(); 
+            SprinklerlengthTB.Text = sprinklerEdit.Length.ToString();
+            SprinklerWeightTB.Text = sprinklerEdit.Weight.ToString();
+        }
+
+        async private void UpdateItemFromTextBoxes_Sprinklers(SprinklerParts sprinklerEdit)
+        {
+            var sprinklerPart = new SprinklerParts(
+                sprinklerEdit.ID,
+                SprinklerCategoryCB.Text,
+                SprinklerPartTB.Text,
+                decimal.Parse(SprinklerCostTB.Text),
+                DateTime.UtcNow,
+                decimal.Parse(SprinklerHeightTB.Text),
+                decimal.Parse(SprinklerwidthTB.Text),
+                decimal.Parse(SprinklerlengthTB.Text),
+                decimal.Parse(SprinklerWeightTB.Text)
+                );
+
+            await _sprinklerPartsRepository.EditSprinklerPart( sprinklerPart );
+            UpdateGrid();
+        }
+
+        private void ClearTextBoxes()
+        {
+            if ((bool)SpinklersParts.IsChecked)
+            {
+                SprinklerCategoryCB.Text = string.Empty;
+                SprinklerPartTB.Text = string.Empty;
+                SprinklerCostTB.Text = string.Empty;
+                SprinklerDatePicker.SelectedDate = DateTime.Now;
+                SprinklerHeightTB.Text = string.Empty;
+                SprinklerwidthTB.Text = string.Empty;
+                SprinklerlengthTB.Text = string.Empty;
+                SprinklerWeightTB.Text = string.Empty;
+            }
+            if ((bool)Pivot.IsChecked)
+            {
+                PivotCategoryCB.Text = string.Empty;
+                PivotPartTB.Text = string.Empty;
+                pivotCostTB.Text = string.Empty;
+                pivotDatePicker.SelectedDate = DateTime.Now;
+                pivotHegitTB.Text = string.Empty;
+                pivotwidthTB.Text = string.Empty;
+                pivotlenghtTB.Text = string.Empty;
+                pivotWeightTB.Text = string.Empty;
+            }
+
+            sprinklerEdit = null;
+            pivotEdit = null;
+            
+        }
+
+        private void pivotPartsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (pivotPartsGrid.SelectedItem is PivotParts selectedItem)
+            {
+                pivotEdit = selectedItem;
+                UpdateTextBoxesFromItem_Pivots(pivotEdit);
+            }
+            else
+            {
+                pivotEdit = null;
+                ClearTextBoxes();
+            }
+        }
+
+        private void UpdateTextBoxesFromItem_Pivots(PivotParts pivotEdit)
+        {
+            PivotCategoryCB.Text = pivotEdit.PivotCategory;
+            PivotPartTB.Text = pivotEdit.PivotPart;
+            pivotCostTB.Text = pivotEdit.Cost.ToString();
+            pivotDatePicker.SelectedDate = pivotEdit.Date;
+            pivotHegitTB.Text = pivotEdit.Height.ToString();
+            pivotwidthTB.Text = pivotEdit.Width.ToString();
+            pivotlenghtTB.Text = pivotEdit.Length.ToString();
+            pivotWeightTB.Text = pivotEdit.Weight.ToString();
+        }
+
+        async private void UpdateItemFromTextBoxes_Pivots(PivotParts pivotEdit)
+        {
+            var pivotPart = new PivotParts(
+                pivotEdit.ID,
+                PivotCategoryCB.Text,
+                PivotPartTB.Text,
+                decimal.Parse(pivotCostTB.Text),
+                DateTime.UtcNow,
+                decimal.Parse(pivotHegitTB.Text),
+                decimal.Parse(pivotwidthTB.Text),
+                decimal.Parse(pivotlenghtTB.Text),
+                decimal.Parse(pivotWeightTB.Text));
+
             await _pivotPartsRepository.EditPivotPart(pivotPart);
-            var result = await _pivotPartsRepository.GetPivotParts();
-            pivotPartsGrid.ItemsSource = result;
+            UpdateGrid();
         }
     }
 }
